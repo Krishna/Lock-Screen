9a5f274fa8de540a690ec9c4690f80496cdf6ee7
Krishna Kotecha <krishna.kotecha@logiccolony.com>
2010-11-27 21:57:20 +0000

    Present lock screen if needed on app start-up Lock the app if needed, when app goes into background

--- Classes/LockScreenAppDelegate.m
+++ Classes/LockScreenAppDelegate.m
@@ -49,11 +49,11 @@
     
     // Add the navigation controller's view to the window and display.
     [self.window addSubview:navigationController.view];
-	
+
 	if ( [self isAppLocked] )
 	{
 		[self lockApplication];
-	}
+	}	
 	
     [self.window makeKeyAndVisible];
 
@@ -65,7 +65,7 @@
     /*
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
+     */	
 }
 
 
@@ -73,7 +73,11 @@
     /*
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
-     */
+     */	
+	if ( [self isAppLocked] )
+	{
+		[self lockApplication];
+	}		
 }
 
 
@@ -87,7 +91,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
+     */	
 }
 
 
